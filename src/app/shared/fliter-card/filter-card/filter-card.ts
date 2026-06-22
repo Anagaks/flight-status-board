df709a8e9ca9fc389d12e6259d8a3005f6f7b990
Anagaks <anaghasgowda2001@gmail.com>
2026-06-22 13:44:20 +0530

    filter component removed

--- src/app/shared/fliter-card/filter-card/filter-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-filter-card',
-  imports: [],
-  templateUrl: './filter-card.html',
-  styleUrl: './filter-card.css',
-})
-export class FilterCard {}
